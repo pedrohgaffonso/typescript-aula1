e0935e7c0d0c1f76514d77cc64da2240d0891811
pedrohgaffonso <pedrohgaffonso@gmail.com>
2022-08-26 20:14:52 -0300

    Enumerated types

--- index.ts
+++ index.ts
@@ -18,3 +18,16 @@ appmeDiv.innerHTML = `
 <h1>Tuple</h1>
 <h2>My name is ${me[0]} and I'm ${me[1]} years old</h2>
 `;
+
+enum Color {Red, Green, Blue};
+let color1: Color = Color.Green;
+
+enum ColorWithRef {Red=10, Green=20, Blue=30}
+let color2: ColorWithRef = ColorWithRef.Blue;
+
+const appcolorDiv: HTMLElement = document.getElementById('appcolor');
+appcolorDiv.innerHTML = `
+<h1>Enumerated types </h1>
+<h2>Cor 1: ${color1}</h2>
+<h2>Cor 2: ${color2}</h2>
+`;
